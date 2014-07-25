73e7d68c4f01097a82da0ab4aeddd80b61a65fbe
Ward9250 <axolotlfan9250@gmail.com>
2014-07-25 20:44:41 +0100

    Started work on Tree Traversers

--- src/phylo/phylo.jl
+++ src/phylo/phylo.jl
@@ -1,5 +1,5 @@
 module Phylo
-
+  import DataStructures: Queue, enqueue!, dequeue!, Stack
   ## Exported Methods and Types
   export PhyExtension,
     PhyNode,
@@ -18,7 +18,11 @@ module Phylo
     setName!,
     setBranchLength!,
     graft!,
-    prune!
+    prune!,
+    searchBF,
+    searchDF,
+    searchAllBF,
+    searchAllDF
 
   ## Load Package Files
   include(Pkg.dir("Bio", "src", "phylo", "typedefs.jl"))
