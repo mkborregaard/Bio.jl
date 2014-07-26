3375c5e36546e9b166c22b944835152fc4df7f9e
Ward9250 <axolotlfan9250@gmail.com>
2014-07-26 01:18:48 +0100

    Altered searching function, added basic idea for abstracting and facilitating tree traversal.

---
+++ src/phylo/Phylo.jl
@@ -0,0 +1,35 @@
+module Phylo
+  import DataStructures: Queue, enqueue!, dequeue!, Stack
+  ## Exported Methods and Types
+  export PhyExtension,
+    PhyNode,
+    Phylogeny,
+    getName,
+    getBranchLength,
+    isLeaf,
+    hasChildren,
+    parentIsSelf,
+    hasParent,
+    getChildren,
+    getSiblings,
+    getParent,
+    isRoot,
+    isNode,
+    setName!,
+    setBranchLength!,
+    graft!,
+    prune!,
+    search,
+    searchAll,
+    TreeTraverser,
+    TraverserDF,
+    TraverserBF,
+    next!,
+    getCurrent,
+    upNext,
+    getHistory,
+    hasReachedEnd
+
+  ## Load Package Files
+  include(Pkg.dir("Bio", "src", "phylo", "typedefs.jl"))
+end
