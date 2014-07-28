38757e42d0c8f3cfab880625f147c99f8a966e07
Ward9250 <axolotlfan9250@gmail.com>
2014-07-28 17:22:55 +0100

    Edited phylo.jj

--- src/phylo/typedefs.jl
+++ src/phylo/typedefs.jl
@@ -25,9 +25,9 @@ before filling in the Parent field with a reference to itself. This means the no
 it could also just be a node that has been created, perhaps in a function, but will be added to another set of nodes subsequently
 in order to build up a tree. Alternatively the user could have just popped it off the tree. I figured a self referential
 node would be the best way to do this rather than have #undef values lurking. It also allows removal of a parent from a node for something like
-say the cutting / pruning of a subtree.
+say the cutting / pruning of a subtree, since you simply need to set the parent field to point to itself, whereas to my knowlege, once a var in Julia
+is set it cannot be made #undef.
  =#
-
 
 # Node constructors.
 function PhyNode(label::String, branchlength::Float64, ext::Array{PhyExtension, 1}, parent::PhyNode)
@@ -119,7 +119,7 @@ function isPreTerminal(x::PhyNode)
   return all([isLeaf(i) for i in x.Children])
 end
 
-# A preterminal node would also return true for this function.
+# A node returning true for isPreTerminal, would also return true for this function.
 function isSemiPreTerminal(x::PhyNode)
   return any([isLeaf(i) for i in x.Children])
 end
@@ -237,23 +237,23 @@ immutable Tip2Root <: PhylogenyIterator
     start::PhyNode
 end
 
-function start(x::DepthFirst)
+function Base.start(x::DepthFirst)
   state = Stack(PhyNode)
   push!(state, x.tree.Root)
   return state
 end
 
-function start(x::BreadthFirst)
+function Base.start(x::BreadthFirst)
   state = Queue(PhyNode)
   enqueue!(state, x.tree.Root)
   return state
 end
 
-function start(x::Tip2Root)
+function Base.start(x::Tip2Root)
   return (x.start, false)
 end
 
-function next(x::DepthFirst, state::Stack{Deque{PhyNode}})
+function Base.next(x::DepthFirst, state::Stack{Deque{PhyNode}})
   current::PhyNode = pop!(state)
   for i in current.Children
     push!(state, i)
@@ -261,7 +261,7 @@ function next(x::DepthFirst, state::Stack{Deque{PhyNode}})
   return current, state
 end
 
-function next(x::BreadthFirst, state::Queue{Deque{PhyNode}})
+function Base.next(x::BreadthFirst, state::Queue{Deque{PhyNode}})
   current::PhyNode = dequeue!(state)
   for i in current.Children
     enqueue!(state, i)
@@ -269,19 +269,19 @@ function next(x::BreadthFirst, state::Queue{Deque{PhyNode}})
   return current, state
 end
 
-function next(x::Tip2Root, state::(PhyNode,Bool))
+function Base.next(x::Tip2Root, state::(PhyNode,Bool))
   return state[1], (state[1].Parent, isRoot(state[1]))
 end
 
-function done(x::DepthFirst, state::Stack{Deque{PhyNode}})
+function Base.done(x::DepthFirst, state::Stack{Deque{PhyNode}})
   return length(state) == 0 ? true : false
 end
 
-function done(x::BreadthFirst, state::Queue{Deque{PhyNode}})
+function Base.done(x::BreadthFirst, state::Queue{Deque{PhyNode}})
   return length(state) == 0 ? true : false
 end
 
-function done(x::Tip2Root, state::(PhyNode,Bool))
+function Base.done(x::Tip2Root, state::(PhyNode,Bool))
   return state[2] 
 end
 
@@ -310,7 +310,8 @@ generateIndex method.
 I'm uncertain whether it is better to get index with a singe search of all the nodes - searchAll, or to do many 
 individual search()-es.
 =#
-function getindex(tree::Phylogeny, names::String...)
+
+function Base.getindex(tree::Phylogeny, names::String...)
   return searchAll(DepthFirstTraverser(tree), x -> in(getName(x), names))
 end
 
