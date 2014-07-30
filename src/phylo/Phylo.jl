c0a5b067c2a415c37af06689deb5174b4e90a5bc
Ward9250 <axolotlfan9250@gmail.com>
2014-07-30 17:39:56 +0100

    Added missing DataStructures to REQUIRE

--- src/phylo/Phylo.jl
+++ src/phylo/Phylo.jl
@@ -2,16 +2,16 @@ module Phylo
 
 using Base.Intrinsics
 
-import Base: getindex, length, start, next, done 
+import Base: getindex, length, start, next, done
 
 import DataStructures: Queue, enqueue!, dequeue!, Stack, Queue, Deque
   ## Exported Methods and Types
-  export PhyNode, getname, getbranchlength, isleaf, haschildren, 
-  parentisself, hasparent, getchildren, getsiblings, getparent, 
+  export PhyNode, getname, getbranchlength, isleaf, haschildren,
+  parentisself, hasparent, getchildren, getsiblings, getparent,
   isroot, isnode, setname!, setbranchlength!, ispreterminal, issemipreterminal,
-  getdescendents, getterminaldescendents, PhyExtension,  Phylogeny, 
+  getdescendents, getterminaldescendents, PhyExtension, Phylogeny, 
   isrooted, isrerootable, setroot!, setrooted!, setrerootable!,
-  graft!, prune!, search, searchall, generateindex, PhylogenyIterator, 
+  graft!, prune!, search, searchall, generateindex, PhylogenyIterator,
   DepthFirst, BreadthFirst, Tip2Root, getmrca
 
   ## Load Package Files
