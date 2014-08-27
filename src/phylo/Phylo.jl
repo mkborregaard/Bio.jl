9b3fa9b0b86c71b7bc8b11a0f724d84664e68404
Ward9250 <axolotlfan9250@gmail.com>
2014-08-27 01:14:30 +0100

    Added function that returns path between two nodes.

--- src/phylo/Phylo.jl
+++ src/phylo/Phylo.jl
@@ -5,16 +5,16 @@ using Base.Intrinsics
 import Base: getindex, length, start, next, done, isempty
 
 import DataStructures: Queue, enqueue!, dequeue!, Stack, Queue, Deque
-  ## Exported Methods and Types
-  export PhyNode, getname, getbranchlength, isleaf, haschildren,
-  parentisself, hasparent, getchildren, getsiblings, getparent,
-  isroot, isnode, setname!, setbranchlength!, ispreterminal, issemipreterminal,
-  getdescendents, getterminaldescendents, countchildren, isintree, PhyExtension,  Phylogeny, 
-  isrooted, isrerootable, root!, setrerootable!,
-  graft!, prune!, pruneregraft!, search, searchall, generateindex, PhylogenyIterator,
-  DepthFirst, BreadthFirst, Tip2Root, getmrca, hasextensions, getroot
+## Exported Methods and Types
+export PhyNode, getname, getbranchlength, isleaf, haschildren,
+parentisself, hasparent, getchildren, getsiblings, getparent,
+isroot, isnode, setname!, setbranchlength!, ispreterminal, issemipreterminal,
+getdescendents, getterminaldescendents, countchildren, isintree, PhyExtension,  Phylogeny,
+isrooted, isrerootable, root!, setrerootable!,
+graft!, prune!, pruneregraft!, search, searchall, generateindex, PhylogenyIterator,
+DepthFirst, BreadthFirst, Tip2Root, getmrca, hasextensions, getroot 
 
-  ## Load Package Files
-  include("typedefs.jl")
-  include("iteration.jl")
+## Load Package Files
+include("typedefs.jl")
+include("iteration.jl")
 end
