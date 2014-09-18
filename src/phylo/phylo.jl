129caf9428c7bf3bbadf8632de94111bd69cf654
Richard Smith-Unna <richardsmith404@gmail.com>
2014-09-18 11:44:24 +0100

    tidy up exports

--- src/phylo/phylo.jl
+++ src/phylo/phylo.jl
@@ -5,14 +5,27 @@ using Base.Intrinsics
 import Base: getindex, length, start, next, done, isempty
 
 import DataStructures: Queue, enqueue!, dequeue!, Stack, Queue, Deque
-## Exported Methods and Types
-export PhyNode, getname, getbranchlength, isleaf, haschildren, 
-parentisself, hasparent, getchildren, getsiblings, getparent, 
-isroot, isnode, setname!, setbranchlength!, ispreterminal, issemipreterminal,
-getdescendents, getterminaldescendents, countchildren, isintree, PhyExtension,  Phylogeny, 
-isrooted, isrerootable, root!, setrerootable!,
-graft!, prune!, pruneregraft!, search, searchall, generateindex, PhylogenyIterator, 
-DepthFirst, BreadthFirst, Tip2Root, getmrca, hasextensions, getroot, pathbetween 
+
+export
+
+  # PhyNode and associated methods
+  PhyNode, getname, getbranchlength, isleaf,
+  haschildren, parentisself, hasparent, getchildren,
+  getsiblings, getparent, isroot, isnode, setname!,
+  setbranchlength!, ispreterminal, issemipreterminal,
+  getdescendents, getterminaldescendents, countchildren,
+  isintree,
+
+  PhyExtension,
+
+  # Phylogeny and associated methods
+  Phylogeny, isrooted, isrerootable, root!,
+  setrerootable!, graft!, prune!, pruneregraft!, search,
+  searchall, generateindex,
+
+  # PhylogenyIterator and associated methods
+  PhylogenyIterator, DepthFirst, BreadthFirst, Tip2Root,
+  getmrca, hasextensions, getroot, pathbetween
 
 ## Load Package Files
 include("typedefs.jl")
