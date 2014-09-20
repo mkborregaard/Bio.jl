264bd03d155be0559e4b68b270956ae81d57138a
Ward9250 <axolotlfan9250@gmail.com>
2014-09-20 22:29:00 +0100

    changes to constructor syntax

--- src/phylo/typedefs.jl
+++ src/phylo/typedefs.jl
@@ -46,7 +46,7 @@ type PhyNode
       x.parent = x
     end
     for child in children
-    graft!(x, child)
+      graft!(x, child)
     end
     return x
   end
@@ -89,7 +89,7 @@ function PhyNode(name::String = "",
   confidence!(x, confidence)
   x.extensions = ext
   if parent != nothing
-    graft!(parent, x)
+    parent_unsafe!(x, x)
   end
   for child in children
     graft!(x, child)
