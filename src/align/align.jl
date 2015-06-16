bc96636b2a77f1795d369b904e9e906ca5e22f9f
Ward9250 <axolotlfan9250@gmail.com>
2015-06-16 20:53:19 +0100

    Moved alternative gaps structure to gaps.jl, defined anchors with sourceSpace, gapSpace and operation fields.

--- src/align/align.jl
+++ src/align/align.jl
@@ -1,12 +1,15 @@
 module Align
 
 using Compat
+using Docile
+using Docile: @doc, @doc_str
 using Base.Intrinsics
 
 import Base: convert, getindex, show, length, start, next, done, copy, reverse,
              show, endof, ==
 
 include("operations.jl")
+include("anchors.jl")
 
 
 end
