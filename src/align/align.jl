ea3842fdb106594b221ca9d4821bd76426879507
Ward9250 <axolotlfan9250@gmail.com>
2015-06-18 02:44:35 +0100

    Added explicit import of lt from Base.Order

--- src/align/align.jl
+++ src/align/align.jl
@@ -4,10 +4,12 @@ using Compat
 using Docile
 using Docile: @doc, @doc_str
 using Base.Intrinsics
-using Base.Order: Ordering, lt
+using Base.Order: Ordering
 
 import Base: convert, getindex, show, length, start, next, done, copy, reverse,
              show, endof, ==, !=, <, >, <=, >=
+
+import Base.Order: lt
 
 include("operations.jl")
 include("anchors.jl")
