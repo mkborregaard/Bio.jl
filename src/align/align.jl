5d443e387171d7f58bdae8020b29d392318ef57a
Ward9250 <axolotlfan9250@gmail.com>
2015-06-18 02:40:47 +0100

    Added custom Ordering types and lt methods for GapAnchors, which hopefully make sorting and searching their arrays fast, whilst avoiding re-writing sort and search methods

--- src/align/align.jl
+++ src/align/align.jl
@@ -4,6 +4,7 @@ using Compat
 using Docile
 using Docile: @doc, @doc_str
 using Base.Intrinsics
+using Base.Order: Ordering, lt
 
 import Base: convert, getindex, show, length, start, next, done, copy, reverse,
              show, endof, ==, !=, <, >, <=, >=
